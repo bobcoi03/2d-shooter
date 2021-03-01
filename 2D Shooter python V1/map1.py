65b4c59a5e268b7ed275230da29c94eb4c05c39e
bobcoi03 <elongatedmusk6969@gmail.com>
2021-03-01 22:31:38 +0700

    map1.py grass,dirt images

---
+++ 2D Shooter python V1/map1.py
@@ -0,0 +1,9 @@
+import os, pygame
+grass_img = pygame.image.load('grass.png')
+dirt_img = pygame.image.load('dirt.png')
+
+grass_img_scale = pygame.transform.scale(grass_img , (50,50))
+dirt_img_scale = pygame.transform.scale(dirt_img, (50,50))
+
+
+				
